57fbf9d1b97fe0d0748b6f9d3f6aeb64f565c180
cinz <seainz@gmail.com>
2019-03-05 20:32:29 +0900

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -5,9 +5,10 @@
 (global-set-key "\M-n" 'scroll-up)
 (global-set-key "\M-p" 'scroll-down)
 ; ctrl+hは１文字削除
-(global-set-key "\^h" 'delete-backward-char) 
+;; (global-set-key "\^h" 'delete-backward-char) 
+(keyboard-translate ?\C-h ?\C-?)
 ; M-hはヘルプ
-(global-set-key "\eh" 'help-for-help)
+; (global-set-key "\eh" 'help-for-help)
 ; M-g で 行Junp
 (global-set-key "\M-g" 'goto-line)
 
@@ -45,3 +46,37 @@
 	 (concat (expand-file-name "~/.saves/")
 		 (file-name-nondirectory file))
 	 ))
+
+;; .emacs に、(show-paren-mode t) と書いておくと、モードにかかわらず、
+;; カーソル位置の括弧に対応する括弧をハイライト表示できるようになり
+;; ます。これは、とっても便利です。
+(show-paren-mode t)
+
+;;; kill ring
+;;; kill ring に関する設定
+(setq kill-ring-max 100)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; http://www.fan.gr.jp/~ring/Meadow/meadow.html
+;;; mini buffer や kill-ring 等の履歴を次回起動時に持ち越したい
+(when (locate-library "session")
+  (require 'session)
+  (setq session-initialize '(de-saveplace session keys menus)
+        session-globals-include '((kill-ring 50)
+                                  (session-file-alist 100 t)
+                                  (file-name-history 100)))
+  (add-hook 'after-init-hook 'session-initialize))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C-. C-, でバッファを切り替える 
+(defun my-select-visible (blst n)
+  (let ((buf (nth n blst)))
+    (cond ((= n (- (length blst) 1)) (other-buffer))
+          ((not (= (aref (buffer-name buf) 0) ? )) buf)
+          (t (my-select-visible blst (+ n 1))))))
+(defun my-grub-buffer ()
+  (interactive)
+  (switch-to-buffer (my-select-visible
+                     (reverse (cdr (assq 'buffer-list (frame-parameters)))) 0)))
+(global-set-key [?\C-,] 'my-grub-buffer)
+(global-set-key [?\C-.] 'bury-buffer)
